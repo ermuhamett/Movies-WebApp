0577116a584e1981631e15dda912bc8211b5ae5a
Ermuhamet <ermyhamet2002@gmail.com>
2024-09-05 16:32:55 +0600

    Implementing multiple auth

---
+++ 1.GettingStarted/Movies.Api/Auth/AdminAuthRequirement.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Movies.Api.Auth;
+
+public class AdminAuthRequirement:IAuthorizationHandler,IAuthorizationRequirement
+{
+    private readonly string _apiKey;
+
+    public AdminAuthRequirement(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        if (!context.User.HasClaim(AuthConstants.AdminUserPolicyName, "true"))
+        {
+            context.Succeed(this);
+            return Task.CompletedTask;
+        }
+
+        var httpContext = context.Resource as HttpContext;
+        if (httpContext is null)
+        {
+            return Task.CompletedTask;
+        }
+        if (!httpContext.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName, out var extractedApiKey))
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+        if (_apiKey != extractedApiKey)
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+
+        var identity = (ClaimsIdentity)httpContext.User.Identity!;
+        identity.AddClaim(new Claim("userid", Guid.NewGuid().ToString()));
+        context.Succeed(this);
+        return Task.CompletedTask;
+    }
+}
