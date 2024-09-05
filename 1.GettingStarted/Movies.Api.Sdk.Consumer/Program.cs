2e1ee0fa568174f26b0c649270116e39b01a03c7
Ermuhamet <ermyhamet2002@gmail.com>
2024-09-05 23:40:58 +0600

    Using the HttpClientFactory

--- 1.GettingStarted/Movies.Api.Sdk.Consumer/Program.cs
+++ 1.GettingStarted/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Refit;
 using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 
-var moviesApi=RestService.For<IMoviesApi>("https://localhost:7026");
+//var moviesApi=RestService.For<IMoviesApi>("https://localhost:7026");
+
+var services = new ServiceCollection();
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(x=>
+        x.BaseAddress=new Uri("https://localhost:7026"));
+
+var provider = services.BuildServiceProvider();
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
 //var movie=await moviesApi.GetMoviesAsync("nick-the-greek-2023");
 
 var request = new GetAllMoviesRequest
