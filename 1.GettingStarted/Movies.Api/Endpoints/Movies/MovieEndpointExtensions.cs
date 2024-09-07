88085f07e886a3ce116fcd778d41204a9d819e3b
Ermuhamet <ermyhamet2002@gmail.com>
2024-09-07 22:38:29 +0600

    Creating the new endpoint structure

---
+++ 1.GettingStarted/Movies.Api/Endpoints/Movies/MovieEndpointExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.Api.Endpoints.Movies;
+
+public static class MovieEndpointExtensions
+{
+    public static IEndpointRouteBuilder MapMovieEndpoints(this IEndpointRouteBuilder app)
+    {
+        /*app.MapCreateMovie();
+        app.MapGetMovie();
+        app.MapGetAllMovies();
+        app.MapUpdateMovies();
+        app.MapDeleteMovies();*/
+        return app;
+    }
+}
